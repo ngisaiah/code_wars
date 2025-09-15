c6b83edc07f7281783d7eb516b06b1b3490131b7
Isaiah Ng <ngisaiah17@gmail.com>
2025-09-15 13:30:58 -0400

    sum positive num

---
+++ javascript/sum_positive.js
@@ -0,0 +1,17 @@
+/*
+DESC:
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example
+[1, -4, 7, 12] => 1+7+12=20
+*/
+
+function positiveSum(arr) {
+  let sum = 0
+  for (i in arr) {
+    if(arr[i] > 0) {
+      sum += arr[i]
+    }
+  }
+  return sum
+}
