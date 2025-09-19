5ab6366245bf05c0a70ecb6f1e2295291525ac54
Isaiah Ng <ngisaiah17@gmail.com>
2025-09-19 13:40:10 -0400

    be concise solutions

---
+++ javascript/concise.js
@@ -0,0 +1,14 @@
+/*
+DESC:
+You are given a function that takes a parameter age (which will always be a positive integer) and does the following:
+
+If the age is 12 or lower, it return "You're a(n) kid"
+If the age is anything between 13 and 17 (inclusive), it return "You're a(n) teenager"
+If the age is anything between 18 and 64 (inclusive), it return "You're a(n) adult"
+If the age is 65 or above, it return "You're a(n) elderly"
+Your task is to shorten the code as much as possible. Note that submitting the given code will not work because there is a character limit of 145.
+*/
+
+function describeAge(a) {
+    return `You're a(n) ${a <= 12 ?'kid':a >= 13&&a <= 17 ?"teenager":a >= 18&&a <= 64 ?"adult":"elderly"}`
+}
