d4cafe807b8e0a4a0abff895896b41b4897cb929
Isaiah Ng <ngisaiah17@gmail.com>
2025-09-29 14:21:41 -0400

    sum arrays

---
+++ python/sum_arr.py
@@ -0,0 +1,12 @@
+'''
+DESC:
+Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+Examples
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+'''
+
+def sum_array(a):
+    total = sum(a)
+    return total
