3c750dd711a80f5b650941ca602d01f96bed68e1
Isaiah Ng <ngisaiah17@gmail.com>
2025-10-01 14:26:10 -0400

    remove first and last char

---
+++ python/remove_first_and_last.py
@@ -0,0 +1,8 @@
+'''
+Your goal is to write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+
+Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+'''
+
+def remove_char(s):
+    return s[1:-1]
