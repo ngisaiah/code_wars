71a0b97999cf2bbace3849a73536ed7316f1b623
Isaiah Ng <ngisaiah17@gmail.com>
2025-10-15 23:17:32 -0400

    unusualFive Solution

---
+++ javascript/unusualFive.js
@@ -0,0 +1,10 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Solution:
+
+function unusualFive() {
+  const arr = ['a','b','c','d','e']
+  return arr.length;
+}
