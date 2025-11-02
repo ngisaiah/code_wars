b3b6cf94487bf32b6aaf656dc89b220d0a4a5850
Isaiah Ng <ngisaiah17@gmail.com>
2025-11-02 12:07:28 -0500

    converts bool to string

---
+++ javascript/convert_bool_to_string.js
@@ -0,0 +1,10 @@
+/*
+DESC:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+function booleanToString(b){
+  return String(b)
+}
