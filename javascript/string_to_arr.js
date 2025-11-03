bcc6c4e01c89e53a8d0a7978497b040b1cf19292
Isaiah Ng <ngisaiah17@gmail.com>
2025-11-03 12:11:10 -0500

    pushes string to array

---
+++ javascript/string_to_arr.js
@@ -0,0 +1,10 @@
+/*
+DSEC: Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+*/
+
+function stringToArray(string){
+  return string.split(' ')
+}
