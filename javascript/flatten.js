0c4dd0c31ccfca90c191d0001bb9ede33f56d925
Isaiah Ng <ngisaiah17@gmail.com>
2025-11-15 02:50:44 -0500

    flatten solution

---
+++ javascript/flatten.js
@@ -0,0 +1,8 @@
+// Desc: Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// Solution:
+
+function flatten(array) {
+  // flat method will flatten 1 level of your array
+    return array.flat();
+}
