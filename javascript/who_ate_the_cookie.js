afea97c5a82c044755a770bf476407143eb664ef
Isaiah Ng <ngisaiah17@gmail.com>
2025-11-28 23:14:18 -0500

    who ate solution

---
+++ javascript/who_ate_the_cookie.js
@@ -0,0 +1,19 @@
+/*
+DESC: For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+*/
+// Solution:
+
+function cookie(x){
+  // Conditional checking data type of x
+  if ((typeof x == typeof 3.14) || (typeof x == typeof 3)) {
+    return "Who ate the last cookie? It was Monica!"
+  } else if (typeof x == typeof 's') {
+    return "Who ate the last cookie? It was Zach!"
+  } else {
+    return "Who ate the last cookie? It was the dog!"
+  }
+}
